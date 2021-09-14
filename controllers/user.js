5c4fb50ac2082ae0f5d5674cd6d197a566d439e0
audace-kwizera <companyfreshbeauty@gmail.com>
2021-09-14 23:58:40 +0200

    création de la logique de connexion de l'utilisateur et connexion login et signin

--- controllers/user.js
+++ controllers/user.js
@@ -13,6 +13,7 @@ exports.signup = (req, res, next) => {
                 email: req.body.email,
                 password: hash
              });
+             /** Sauvegarde dans la base de donnée */
              user.save()
                 .then(() => res.status(201).json({ message: string }))
                 .catch(error => res.status(400).json({ error }));
@@ -22,6 +23,25 @@ exports.signup = (req, res, next) => {
 
 /** Middleware connexion utilisateur */
 exports.login = (req, res, next) => {
-
+    /** Trouver l'user dans la db */
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(!user) {
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+            }
+            /** Comparaison du mot de passe envoyé dans la requête avec le hash enregistré dans la db */
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ error: 'Mot de passe incorrect' });
+                    }
+                    res.status(200).json({
+                        userId: user._id,
+                        token: 'TOKEN'
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 

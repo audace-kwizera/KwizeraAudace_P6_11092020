d30a8c15d4068485045bf1994bd180ba8b5af147
audace-kwizera <companyfreshbeauty@gmail.com>
2021-09-14 23:45:00 +0200

    création et sauvegarde de nouvel utilisateur

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,23 @@
+/** Importation du hash */
+const bcrypt = require('bcrypt');
+
 /**Importation du modèle user */
 const User = require('../models/User');
 
 /** Middleware enregistrement nouvel utilisateur */
 exports.signup = (req, res, next) => {
-
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            /** Création de l'utilisateur */
+            const user = new User({ 
+                email: req.body.email,
+                password: hash
+             });
+             user.save()
+                .then(() => res.status(201).json({ message: string }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
 /** Middleware connexion utilisateur */
